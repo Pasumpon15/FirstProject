cdd61271e1bce21ec9e1284c4d2c5c609d66df9e
Shiyamala25 <shiyamalamphil.maths@gmail.com>
2021-10-09 20:06:58 +0530

    sample

--- src/test/java/org/test/BrowserLaunch.java
+++ src/test/java/org/test/BrowserLaunch.java
@@ -7,8 +7,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserLaunch {
 	public static void main(String[] args) {
+		System.out.println ("Hi");
 		WebDriverManager.chromedriver().setup();
+
 		WebDriver driver = new ChromeDriver();
+
 		driver.get("https://en-gb.facebook.com/");
 	    String title = driver.getTitle();
 	    System.out.println(title);
