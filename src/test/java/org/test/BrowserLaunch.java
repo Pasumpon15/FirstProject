5b8a15c735d4e35c39604e19d0b02957898733f4
Shiyamala25 <shiyamalamphil.maths@gmail.com>
2021-10-09 20:36:14 +0530

    New commit

--- src/test/java/org/test/BrowserLaunch.java
+++ src/test/java/org/test/BrowserLaunch.java
@@ -8,10 +8,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BrowserLaunch {
 	public static void main(String[] args) {
 		System.out.println ("Hi");
+		System.out.println ("Hii");
+		System.out.println ("Hiii");
 		WebDriverManager.chromedriver().setup();
-
 		WebDriver driver = new ChromeDriver();
-
 		driver.get("https://en-gb.facebook.com/");
 	    String title = driver.getTitle();
 	    System.out.println(title);
